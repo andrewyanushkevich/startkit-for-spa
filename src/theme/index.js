a06ef6370ca2b0acc53a85c5f2d46a0e651be382
u-yaudoshchanka <u.yaudoshchanka@gmail.com>
2019-09-24 16:18:09 +0300

    fix font sizes

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,9 @@
 export default {
-  functionTitle: '15rem',
-  gridHeader: '10rem',
-  gridText: '10rem',
-  formTitle: '15rem',
-  formHeader: '15rem',
-  formText: '10rem',
-  buttonText: '10rem',
+  functionTitle: '15px',
+  gridHeader: '10px',
+  gridText: '10px',
+  formTitle: '15px',
+  formHeader: '15px',
+  formText: '10px',
+  buttonText: '10px',
 };
